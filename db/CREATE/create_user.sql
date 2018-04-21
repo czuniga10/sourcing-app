13629b30be3a2c57568f2e137ef76d176727fea4
Chad <chad.zuniga10@gmail.com>
2018-04-20 18:50:53 -0600

    get user by id and register user routers tested and working

---
+++ db/CREATE/create_user.sql
@@ -0,0 +1,3 @@
+insert into users(username, first_name, last_name, email, password, company)
+values ( $1, $2, $3, $4, $5, $6 )
+;
--- server/routers/auth.router.js
+++ server/routers/auth.router.js
@@ -2,7 +2,7 @@ const express = require('express');
 const getDb = require('../database/bootstrap.database');
 
 const authRouter = express.Router();
-
+//test worked
 authRouter.get('/:id', (req, res) => {
     const id = req.params.id;
     const db = getDb();
@@ -10,13 +10,13 @@ authRouter.get('/:id', (req, res) => {
         .then( user => res.status(200).send(user))
         .catch( err => res.status(500).send(err))
 });
-
+//test worked
 authRouter.post('/register', (req, res) => {
     const db = getDb();
-    const { first_name, last_name, company, address, city, state, zip_code, phone, email } = req.body;
-    db.create_user([first_name, last_name, company, address, city, state, zip_code, phone, email])
+    const { username, first_name, last_name, email, password, company } = req.body;
+    db.CREATE.create_user([ username, first_name, last_name, email, password, company ])
         .then( () => res.status(200).send())
-        .catch( err => res.send(err))
+        .catch( err => res.status(500).send(err))
 });
 
 authRouter.put('/update/:id', (req, res) => {
@@ -25,14 +25,14 @@ authRouter.put('/update/:id', (req, res) => {
     const { first_name, last_name, company, address, city, state, zip_code, phone, email } = req.body;
     db.update_user([id, first_name, last_name, company, address, city, state, zip_code, phone, email])
         .then( promise => res.status(200).send(promise))
-        .catch( err => res.send(err) )
+        .catch( err => res.status(500).send(err) )
 });
 
 authRouter.delete('/delete/:id', (req, res) => {
     const db = getDb();
     db.delete_user(req.params.id)
         .then( () => res.status(200).send() )
-        .catch( err => res.send(err) )
+        .catch( err => res.status(500).send(err) )
 });
 
 module.exports = authRouter;
