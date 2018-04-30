f65a008ce18bfec43e91362d0ef0efb8673c20a2
Chad <chad.zuniga10@gmail.com>
2018-04-30 17:47:26 -0600

    create react app created

--- server/routers/delegate.routers.js
+++ server/routers/delegate.routers.js
@@ -1,19 +1,17 @@
-const authRouter = require('./auth.router');
+// const authRouter = require('./auth.router');
 const projectRouter = require('./project.router');
 const cardRouter = require('./card.router');
-
+const userRouter = require('./user.router')
 
 
 function delegateRoutes(app) {
-    app.use('/api/auth', authRouter);
+    app.use('/api/user', userRouter);
     app.use('/api/project', projectRouter);
     app.use('/api/card', cardRouter);
+    // app.use('/api/auth', authRouter);
     
 
-    app.all('*', (req, res) => {
-        // console.log('unknown route ' + req.url);
-        res.status(404).send({message: 'Cannot access any resource at ' + req.originalUrl});
-    });
+    
     
     return app;
 }
---
+++ server/routers/user.router.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const getDb = require('../database/bootstrap.database');
+
+const userRouter = express.Router();
+//test worked
+userRouter.get('/:id', (req, res) => {
+    const id = req.params.id;
+    const db = getDb();
+    db.READ.get_user( [id] )
+        .then( user => res.status(200).send(user))
+        .catch( err => res.status(500).send(err))
+});
+//test worked
+userRouter.post('/register', (req, res) => {
+    const db = getDb();
+    const { username, first_name, last_name, email, password, company } = req.body;
+    db.CREATE.create_user([ username, first_name, last_name, email, password, company ])
+        .then( (user) => res.status(200).send(user))
+        .catch( err => res.status(500).send(err))
+});
+//test worked
+userRouter.put('/update/:id', (req, res) => {
+    const db = getDb();
+    const id = req.params.id;
+    const { username, first_name, last_name, email, password, company } = req.body;
+    db.UPDATE.update_user([id, username, first_name, last_name, email, password, company])
+        .then( promise => res.status(200).send(promise))
+        .catch( err => res.status(500).send(err) )
+});
+//test works ... updates status_id = 2
+userRouter.put('/delete/:id', (req, res) => {
+    const db = getDb();
+    db.DELETE.delete_user(req.params.id)
+        .then( () => res.status(200).send() )
+        .catch( err => res.status(500).send(err) )
+});
+
+module.exports = userRouter;
